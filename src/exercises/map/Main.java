e6d1bc20ba4d97361f176726c02e0c5b39060ab5
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-06 08:48:36 -0300

    Finish map exercise

--- src/exercises/map/Main.java
+++ src/exercises/map/Main.java
@@ -1,10 +1,43 @@
 package exercises.map;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		Map<String, Integer> voteRecords = new HashMap<>();
+		System.out.println("Type the file full path: ");
+		String path = sc.nextLine();
+		///home/arthur/Área de trabalho/
+		
+		try(BufferedReader br = new BufferedReader( new FileReader(path))){
+			String line = br.readLine();
+			while(line !=null) {
+				String[] fields =  line.split(",");
+				
+				String name = fields[0];
+				Integer votes = Integer.parseInt(fields[1]);
+				
+				if (voteRecords.containsKey(name)) {
+			        int currentVotes = voteRecords.get(name);
+			        voteRecords.put(name, currentVotes + votes);
+			    } else {
+			        voteRecords.put(name, votes);
+			    }
+				line = br.readLine(); 
+			}
+		}
+		catch(IOException ex) {
+			System.out.println("Error: " + ex.getMessage());
+		}
+		
+		System.out.println(voteRecords);
 	}
 
 }
